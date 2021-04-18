31ca907472e01a267a8a20659cb960c2bfab2676
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-04-18 23:48:25 +0200

    Build iwth rollup

--- podlet-b/snowpack.config.js
+++ podlet-b/snowpack.config.js
@@ -1,24 +1,45 @@
-/** @type {import("snowpack").SnowpackUserConfig } */
+const resolve = require('@rollup/plugin-node-resolve').default;
+const commonjs = require('@rollup/plugin-commonjs');
+
 module.exports = {
   mount: {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    [
+      'snowpack-plugin-rollup-bundle',
+      {
+        entrypoints: 'build/dist/index.js',
+
+        extendConfig: (config) => {
+          config.outputOptions.dir = 'build/dist';
+          config.outputOptions.format = 'esm';
+
+          return config;
+        },
+      },
+    ],
+  ],
   routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    /* ... */
   ],
   optimize: {
     bundle: true,
   },
   packageOptions: {
-    /* ... */
+    NODE_ENV: true,
+    rollup: {
+      plugins: [commonjs(), resolve()],
+    },
   },
   devOptions: {
-    /* ... */
+    open: 'none',
+    bundle: true,
   },
   buildOptions: {
-    /* ... */
+    clean: true,
   },
 };
