d4fd118b37753cd88f4777c5b7ff4c2dc59170a8
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-04-18 21:52:21 +0200

    Set origin header on the requested resource

--- podlet-b/podlet.js
+++ podlet-b/podlet.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Podlet = require('@podium/podlet');
+var cors = require('cors');
 
 const podlet = new Podlet({
   name: 'podlet-b',
@@ -10,13 +11,12 @@ const podlet = new Podlet({
   development: true,
 });
 
-podlet.css({ value: '/build/dist/index.css' });
-podlet.js({ value: '/build/dist/index.js', type: 'module' });
-
 const app = express();
-app.use(podlet.middleware());
+app.use(cors({ origin: 'http://localhost:7000' }));
 
 app.use('/build/dist', express.static('./build/dist'));
+
+app.use(podlet.middleware());
 
 app.get(podlet.content(), (req, res) => {
   res.status(200).podiumSend(`
@@ -27,5 +27,8 @@ app.get(podlet.content(), (req, res) => {
 app.get(podlet.manifest(), (req, res) => {
   res.status(200).send(podlet);
 });
+
+podlet.css({ value: '/build/dist/index.css' });
+podlet.js({ value: '/build/dist/index2.js', type: 'module' });
 
 app.listen(7200, () => console.log('Podlet listening on port 7200'));
