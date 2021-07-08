331c10c7f5db764cc3f089b5e9a29f2887cea762
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-07-08 18:40:12 +0200

    Add snowpack to podlet

--- podlet-a/podlet.js
+++ podlet-a/podlet.js
@@ -1,28 +1,26 @@
-const express = require("express");
-const Podlet = require("@podium/podlet");
+const express = require('express');
+const Podlet = require('@podium/podlet');
+var cors = require('cors');
 
 const podlet = new Podlet({
-  name: "podlet-a",
-  version: "1.0.0",
-  pathname: "/",
-  content: "/",
-  fallback: "/fallback",
+  name: 'podlet-a',
+  version: '1.0.0',
+  pathname: '/',
+  content: '/',
+  fallback: '/fallback',
   development: true,
 });
 
-podlet.css({ value: "/dist/index.css" });
-podlet.js({ value: "/dist/index.js" });
-
 const app = express();
-app.use(podlet.middleware());
+app.use(cors({ origin: 'http://localhost:7000' }));
 
-app.use("/dist", express.static("./dist"));
+app.use('/build/dist', express.static('./build/dist'));
+
+app.use(podlet.middleware());
 
 app.get(podlet.content(), (req, res) => {
   res.status(200).podiumSend(`
-        <div class="podlet-a">
-            This is the podlet-a content
-        </div>
+        <div id="root"></div>
     `);
 });
 
@@ -30,4 +28,7 @@ app.get(podlet.manifest(), (req, res) => {
   res.status(200).send(podlet);
 });
 
-app.listen(7100, () => console.log("Podlet listening on port 7100"));
+podlet.css({ value: '/build/dist/index.css' });
+podlet.js({ value: '/build/dist/index.js', type: 'module' });
+
+app.listen(7100, () => console.log('Podlet listening on port 7100'));
---
+++ podlet-a/snowpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+  ],
+  routes: [
+    /* ... */
+  ],
+  optimize: {
+    bundle: true,
+    sourcemap: false,
+  },
+  packageOptions: {
+    NODE_ENV: true,
+  },
+  devOptions: {
+    open: 'none',
+    bundle: true,
+  },
+  buildOptions: {
+    clean: true,
+  },
+};
