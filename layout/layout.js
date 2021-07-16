64a672c59b6f2f5ed49ca1ad726e5ad19bff1133
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-07-16 10:17:49 +0200

    Remove resolve option

--- layout/layout.js
+++ layout/layout.js
@@ -13,22 +13,16 @@ const layout = new Layout({
 const podletA = layout.client.register({
   name: "podlet-a",
   uri: "http://localhost:7100/manifest.json",
-  resolveJs: true,
-  resolveCss: true,
 });
 
 const podletB = layout.client.register({
   name: "podlet-b",
   uri: "http://localhost:7200/manifest.json",
-  resolveJs: true,
-  resolveCss: true,
 });
 
 const podletC = layout.client.register({
   name: "podlet-c",
   uri: "http://localhost:7300/manifest.json",
-  resolveJs: true,
-  resolveCss: true,
 });
 
 app.use(layout.middleware());
